41cb6de6e0e296f7e3ffe1b99e395acb243618e3
elvin <pa908tkpe@gmail.com>
2021-05-04 14:10:24 +0800

    first learn

---
+++ Conversion類型轉換.cs
@@ -0,0 +1,26 @@
+﻿using System ;
+namespace RectangleApplication
+{
+      class ExecuteRectangle
+    {
+        static void Main ( string [ ] args )
+        {
+            string s = "1234" ; 
+            int it = 0 ; 
+            it = int.Parse(s) ; 
+            Console.WriteLine(it) ; 
+
+            int i = 75 ;
+            float f = 53.005f ;
+            double d = 2345.7652 ;
+            bool b = true ;
+
+            Console.WriteLine ( i.ToString ( ) ) ;
+            Console.WriteLine ( f.ToString ( ) ) ;
+            Console.WriteLine ( d.ToString ( ) ) ;
+            Console.WriteLine ( b.ToString ( ) ) ;
+            Console.ReadKey ( ) ;
+
+        }
+    }
+}
---
+++ class_OOP.cs
@@ -0,0 +1,29 @@
+﻿using System ;
+namespace RectangleApplication
+{
+    class ExecuteRectangle
+    {
+        public int FindMax ( int num1, int num2 )
+        {
+            /*局部變量聲明*/
+            int result ;
+
+            if ( num1 > num2 )
+                result = num1 ;
+            else
+                result = num2 ;
+            return result ;
+        }
+    }
+
+    class Test 
+    {
+        static void Main ( string [ ] args )
+        {
+            ExecuteRectangle n = new ExecuteRectangle() ; 
+            int i = n.FindMax(1,2) ; 
+            Console.WriteLine(i) ; 
+        }
+
+    }
+}
